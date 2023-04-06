7216f6db1f6058357c0ecc9ea4100f0f66cad355
Rina Yang <yangsabr@usc.edu>
2023-04-06 22:17:01 +0000

    hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,6 +20,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool solveRecursive( const AvailabilityMatrix avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t>& shifts, int day, int slot);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -36,9 +37,62 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+	for (int i = 0; i < avail.size(); i++){
+		vector<Worker_T> row(dailyNeed, -1);
+		sched.push_back(row);
+	}
+	AvailabilityMatrix aCopy=avail;
 
 
-
-
+	
+	vector<size_t> shifts( avail[0].size(), 0);
+	return solveRecursive( aCopy, dailyNeed, maxShifts, sched, shifts, 0, 0);	// add int day = 0 and int slot = 0
 }
+
+bool solveRecursive( AvailabilityMatrix avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t>& shifts, int day, int slot)
+{
+	
+  if( dailyNeed == 0) return true;
+	if( avail.size() == 0) return false;
+
+	const size_t availworkers = avail[0].size();
+
+	
+	if(day==sched.size()){
+		return true;
+	}
+
+
+	for( size_t i = 0; i<availworkers; i++ )
+	{
+		if( avail[day][i] && shifts[i] < maxShifts )
+		{
+			sched[day][slot] = i;
+			shifts[i]++;
+			avail[day][i]=0;
+
+			
+			if(slot==dailyNeed-1){
+        if(solveRecursive( avail, dailyNeed, maxShifts, sched, shifts, day+1,0 ) ){
+          return true;
+        }
+			shifts[i]--;
+      sched[day][slot] = -1;
+			avail[day][i]=1;
+      }
+      else{
+        if(solveRecursive( avail, dailyNeed, maxShifts, sched, shifts, day,slot+1 )){
+          return true;
+        }
+				shifts[i]--;
+     	  sched[day][slot] = -1;
+				avail[day][i]=1;
+			}
+			
+		}
+	}
+
+	return false;
+}
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,16 +13,62 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+// std::set<std::string> assignFloat(std::string in, std::set<std::string> setIn, const std::set<std::string>& dic, std::string floating, int index);
+// std::set<std::string> assignChar(std::string in, std::set<std::string> setIn, const std::set<std::string>& dic);
+// bool find(std::string& in, const std::set<std::string>& dic);
+void helper(std::string in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& sets, const unsigned int c);
+void helper2( const unsigned int c, std::string in, const std::set<std::string>& dict, std::set<std::string>& sets);
 
-
-// Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+
+    std::set<std::string> sets;
+    helper(in, floating, dict, sets, 0);
+    return sets;
 
 }
+void helper2(const unsigned int c, std::string in, const std::set<std::string>& dict, std::set<std::string>& sets)
+{
+	if(c == in.length() )
+	{
+		if( dict.find(in) != dict.end() )
+		{
+			sets.insert(in); 
+			return; 
+		}
+	}
+	else if( in[c] == '-' )
+	{
+		for (int i=97; i<123; i++)
+		{
+			in[c] = char(i);
+			helper2(  c+1, in, dict, sets );
+		}
+	}
+	else
+	{
+		helper2(  c+1, in, dict, sets );
+	}
+}
+void helper(std::string in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& sets, const unsigned int c)
+{
+	if( floating.length() ==c)
+	{
+		helper2(0,  in, dict, sets );
+		return;
+	}
 
-// Define any helper functions here
+	for(  int i = 0; i < (int)in.length(); i++ )
+	{
+		if( in[i] == '-' )
+		{
+			in[i] = floating[c];
+			helper(in, floating, dict, sets, c+1 );//fill in the rest
+			in[i] = '-';//to keep recursion
+		}
+	}
+}
+
